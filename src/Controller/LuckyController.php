aba5f2b45dcc3f41c249ddcb4b556ca7d11c3b73
CyndelHerolt <herolt.cyndel@gmail.com>
2022-06-04 14:52:53 +0200

    first loop "for" in symfony

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -10,14 +10,45 @@ class LuckyController extends AbstractController
     /**
      * @Route("/lucky/number", name="app_lucky_number")
      */
-    public function number()
+    function News()
     {
-        $number = random_int(0, 100);
+        $actualites = [
+            [
+                'titre' => 'Concours plaidoiries 2022',
+                'date' => '25/05/2022',
+                'image' => 'https://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-plaidoiries2022.jpg'
+            ],
+            [
+                'titre' => 'Rénovation énergétique de l\'IUT',
+                'date' => '20/05/2022',
+                'image' => 'https://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-Actu-renovationenergetique2022.jpg' //image à remplacer
+            ],
+            [
+                'titre' => 'Afterwork GMP & futur Job Dating',
+                'date' => '18/05/2022',
+                'image' => 'https://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-afterworkGMP-mai2022.jpg' //image à remplacer
+            ],
+            [
+                'titre' => 'L4M : Recrutements Experts',
+                'date' => '16/05/2022',
+                'image' => 'https://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-salonL4M-juin2022.jpg' //image à remplacer
+            ],
+            [
+                'titre' => 'CROUS : ateliers d\'éloquence',
+                'date' => '27/04/2022',
+                'image' => 'https://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-Atelierseloquences-CROUS2022.jpg' //image à remplacer
+            ],
+            [
+                'titre' => 'Vacances de Printemps : fermeture de l\'IUT',
+                'date' => '08/04/2022',
+                'image' => 'https://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-vacancesprintemps.jpg' //image à remplacer
+            ],
+        ];
 
         return $this->render(
             'number.html.twig',
             [
-                'number' => $number
+                'news' => $actualites,
             ]
         );
     }

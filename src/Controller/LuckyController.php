5c6a76c79293d9144b374b9979762fc9700e4a95
CyndelHerolt <herolt.cyndel@gmail.com>
2022-06-04 11:41:52 +0200

    Header construction

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number", name="app_lucky_number")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render(
+            'number.html.twig',
+            [
+                'number' => $number
+            ]
+        );
+    }
+}
